1c9c8efb303be3f4c030780e311841347171f00f
Hazem Magdy <hhazem0010@gmail.com>
2024-05-10 22:24:45 +0300

    Caching - fix task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
+"""Module to represent a caching system."""
 
-BaseCaching = __import__('base_caching').BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class BasicCache(BaseCaching):
-    super.__init__()
+    """class for basic caching implementation."""
 
     def put(self, key, item):
+        """Add an item to the cache."""
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Get an item by key from cache."""
         if key in self.cache_data:
             return self.cache_data[key]
         else:
