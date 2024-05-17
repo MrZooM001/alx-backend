c9c4c1bfb251ab7f87494bdeb5fdd91971b8f1f7
Hazem Magdy <hhazem0010@gmail.com>
2024-05-17 21:51:50 +0300

    task 3- Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Module for creating basic Flask Application"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Class for app configurations"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Function to determines the best language match for the user's browser
+    settings based on the app's configured languages.
+
+    Returns:
+        str: The best matching language code, or None if no match is found.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index() -> str:
+    """
+    Function as a route handler for the root ("/") URL.
+    It returns the rendered template '3-index.html'.
+
+    Returns:
+        str: The rendered HTML content of '3-index.html' template.
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- 0x02-i18n/README.md
+++ 0x02-i18n/README.md
@@ -5,5 +5,11 @@
 * **[0. Basic Flask app](0-app.py)**
 * **[1. Basic Babel setup](1-app.py)**
 * **[2. Get locale from request](2-app.py)**
+* **[3. Parametrize templates](3-app.py)**
+* **[4. Force locale with URL parameter](4-app.py)**
+* **[5. Mock logging in](5-app.py)**
+* **[6. Use user locale](6-app.py)**
+* **[7. Infer appropriate time zone](7-app.py)**
+* **[8. Display the current time](app.py)**
 
 
